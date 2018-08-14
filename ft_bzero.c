5f4d32d03639cb3e3eaacfde01c484308d5df367
Oksanatishka <Oksanatishka@users.noreply.github.com>
2018-08-14 14:27:14 -0700

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,35 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obibik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/14 13:53:28 by obibik            #+#    #+#             */
+/*   Updated: 2018/08/14 13:53:38 by obibik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
-* bzero -- write zeroes to a byte string
-* The bzero() function writes n zeroed bytes to the string s.  If n is zero, bzero() does nothing.
+** bzero -- write zeroes to a byte string
+** The bzero() function writes n zeroed bytes to the string s.
+** If n is zero, bzero() does nothing.
 */
 
-void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-	unsigned char *p = s;
+	char *p;
 
+	p = (char *)b;
 	while (n > 0)
 	{
 		*p = 0;
 		p++;
 		n--;
 	}
-	return;
+	return ;
 }
 
 /*
-* void	ft_bzero(void *s, size_t n)
-* {
-* 	ft_memset(s, 0, n);
-* }
+** void	ft_bzero(void *s, size_t n)
+** {
+** 	ft_memset(s, 0, n);
+** }
 */
 
 /*
-* int main()
-* {
-*    char str[100] = "Libc is the standard library for the C programming language.";
-*
-*    ft_bzero(str + 3, 1);
-*    printf("After memset(): %s\n", str);
-*    return 0;
-* }
+** int main()
+** {
+**    char str[100] = "Libc is the standard library.";
+**
+**    ft_bzero(str + 3, 1);
+**    printf("After memset(): %s\n", str);
+**    return 0;
+** }
 */
