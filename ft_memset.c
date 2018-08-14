accf0b9f9cc0f1973e06c554652dc67f2bb09ff3
Oksanatishka <Oksanatishka@users.noreply.github.com>
2018-08-14 13:46:33 -0700

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,13 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obibik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/14 12:12:09 by obibik            #+#    #+#             */
+/*   Updated: 2018/08/14 13:08:57 by obibik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
-* memset -- fill a byte string with a byte value
-* The memset() function writes len bytes of value c (converted to an unsigned char) to the string b.
-* The memset() function returns its first argument.
+** memset -- fill a byte string with a byte value.
+** The memset() function writes len bytes of value c
+** (converted to an unsigned char) to the string b.
+** The memset() function returns its first argument.
 */
 
-void *ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char *p = b;
+	char *p;
 
+	p = (char *)b;
 	while (len > 0)
 	{
 		*p = c;
@@ -18,12 +32,12 @@ void *ft_memset(void *b, int c, size_t len)
 }
 
 /*
-* int main()
-* {
-* 	char str[100] = "Libc is the standard library for the C programming language.";
-*
-*	ft_memset(str, '.', 8 * sizeof(char));
-*	printf("After memset(): %s\n", str);
-*	return 0;
-* } 
+** int main()
+** {
+** 	char str[100] = "Libc is the standard library.";
+**
+**	ft_memset(str, '.', 8 * sizeof(char));
+**	printf("After memset(): %s\n", str);
+**	return 0;
+** }
 */
