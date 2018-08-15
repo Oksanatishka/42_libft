c44434da7852cf0653d9dc304bf24417b87ad363
Oksanatishka <Oksanatishka@users.noreply.github.com>
2018-08-14 18:56:35 -0700

    Create ft_toupper.c

---
+++ ft_toupper.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obibik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/14 17:36:47 by obibik            #+#    #+#             */
+/*   Updated: 2018/08/14 17:36:50 by obibik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** toupper, toupper_l -- lower case to upper case letter conversion
+*/
+
+#include <stdio.h>
+#include "libft.h"
+
+int		ft_toupper(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (c - 32);
+	return (c);
+}
+
+/*
+** int main()
+** {
+** 		char c, result;
+**
+**      c = 'M';
+**  	result = ft_toupper(c);
+**      printf("The result is %c\n", result);
+**
+**      c = 'g';
+**  	result = ft_toupper(c);
+**      printf("The result is %c\n", result);
+**
+**      c = '+';
+**  	result = ft_toupper(c);
+**      printf("The result is %c\n", result);
+**
+**  	return 0;
+** }
+*/
