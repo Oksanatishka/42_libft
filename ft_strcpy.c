e930bb83e2f6a592ffc28b4206eedf74fa6ec710
Oksanatishka <Oksanatishka@users.noreply.github.com>
2018-08-15 15:53:06 -0700

    Update ft_strcpy.c

---
+++ ft_strcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obibik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/14 17:30:26 by obibik            #+#    #+#             */
+/*   Updated: 2018/08/14 17:30:29 by obibik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** stpcpy, stpncpy, strcpy, strncpy -- copy strings
+*/
+
+#include "libft.h"
+
+char	*ft_strcpy(char *dst, const char *src)
+{
+	int		i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = src[i];
+	return (dst);
+}
+
+/*
+** int main ()
+** {
+**     char example[50];
+**
+**     strcpy(example, "Life is good!");
+**     printf("%s\n", example);
+**
+**    return(0);
+** }
+*/
