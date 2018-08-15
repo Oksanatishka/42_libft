d28b74424abea55d4e789cd2244ed46e32a057d8
Oksanatishka <Oksanatishka@users.noreply.github.com>
2018-08-15 16:53:41 -0700

    Update ft_strncpy.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -1,15 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obibik <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/14 17:32:09 by obibik            #+#    #+#             */
+/*   Updated: 2018/08/14 17:32:11 by obibik           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
-* strncpy() function copy at most len characters from src into dst.
-* strncpy() function return dst.
+** stpcpy, stpncpy, strcpy, strncpy -- copy strings
 */
 
-char *strncpy(char * dst, const char * src, size_t len)
-{
+#include "libft.h"
 
+char	*ft_strncpy(char *dest, const char *src, size_t len)
+{
+	size_t i;
+
+	i = 0;
+	while (src[i] != '\0' && (i < len))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < len)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
 
-int main()
-{
-	
-	return 0;
-}
+/*
+**  int main ()
+**  {
+**      char example[50];
+**
+**      ft_strncpy(example, "Life is good!", 9);
+**      printf("%s\n", example);
+**
+**     return(0);
+**  }
+*/
